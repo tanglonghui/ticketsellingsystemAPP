d6b0cff482a51b4a0e03972e3c248aafe03b4400
tanglonghui <1776234240@qq.com>
2019-04-15 15:23:11 +0800

    12.0 小锤

--- app/src/main/java/org/ironman/ticketsellingsystem/ui/activity/ChangePasswordActivity.java
+++ app/src/main/java/org/ironman/ticketsellingsystem/ui/activity/ChangePasswordActivity.java
@@ -1,27 +1,60 @@
 package org.ironman.ticketsellingsystem.ui.activity;
 
-        import android.app.Activity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import org.ironman.ticketsellingsystem.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import cn.droidlover.xdroidmvp.mvp.XActivity;
 import cn.droidlover.xdroidmvp.router.Router;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * Created by Archer on 2019/3/17.
  */
 
-public class ChangePasswordActivity  extends XActivity implements View.OnClickListener {
+public class ChangePasswordActivity extends XActivity implements View.OnClickListener {
+    @BindView(R.id.content_back)
+    TextView contentBack;
+    @BindView(R.id.content_title)
+    TextView contentTitle;
+    @BindView(R.id.iv_head)
+    CircleImageView ivHead;
+    @BindView(R.id.tv_name)
+    TextView tvName;
+    @BindView(R.id.tv_account)
+    TextView tvAccount;
+    @BindView(R.id.rl_head)
+    RelativeLayout rlHead;
+    @BindView(R.id.et_password)
+    EditText etPassword;
+    @BindView(R.id.et_reset_password)
+    EditText etResetPassword;
+    @BindView(R.id.et_check_password)
+    EditText etCheckPassword;
+    @BindView(R.id.tv_login)
+    TextView tvLogin;
+
     @Override
     public void onClick(View v) {
-
+        switch (v.getId()) {
+            case R.id.content_back:
+                finish();
+                break;
+        }
     }
 
     @Override
     public void initData(Bundle savedInstanceState) {
-
+        contentTitle.setText("修改密码");
+        contentBack.setVisibility(View.VISIBLE);
+        contentBack.setOnClickListener(this);
     }
 
     @Override
@@ -38,8 +71,15 @@ public class ChangePasswordActivity  extends XActivity implements View.OnClickLi
     protected void oneLogin(String msg) {
 
     }
+
     public static void launch(Activity activity) {
         Router.newIntent(activity).to(ChangePasswordActivity.class).launch();
     }
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // TODO: add setContentView(...) invocation
+        ButterKnife.bind(this);
+    }
 }
--- app/src/main/java/org/ironman/ticketsellingsystem/ui/fragment/MyFragment.java
+++ app/src/main/java/org/ironman/ticketsellingsystem/ui/fragment/MyFragment.java
@@ -1,17 +1,47 @@
 package org.ironman.ticketsellingsystem.ui.fragment;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import org.ironman.ticketsellingsystem.MainActivity;
 import org.ironman.ticketsellingsystem.R;
+import org.ironman.ticketsellingsystem.ui.activity.ChangePasswordActivity;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 import cn.droidlover.xdroidmvp.mvp.XFragment;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 
-public class MyFragment extends XFragment {
+public class MyFragment extends XFragment implements View.OnClickListener {
+
+    @BindView(R.id.iv_head)
+    CircleImageView ivHead;
+    @BindView(R.id.tv_name)
+    TextView tvName;
+    @BindView(R.id.tv_account)
+    TextView tvAccount;
+    @BindView(R.id.rl_head)
+    RelativeLayout rlHead;
+    @BindView(R.id.tv_add_pasenger)
+    TextView tvAddPasenger;
+    @BindView(R.id.tv_change_password)
+    TextView tvChangePassword;
+    @BindView(R.id.tv_logout)
+    TextView tvLogout;
+    Unbinder unbinder;
 
     @Override
     public void initData(Bundle savedInstanceState) {
-
+        tvLogout.setOnClickListener(this);
+        tvChangePassword.setOnClickListener(this);
+        tvAddPasenger.setOnClickListener(this);
+        rlHead.setOnClickListener(this);
     }
 
     @Override
@@ -27,5 +57,38 @@ public class MyFragment extends XFragment {
     @Override
     protected void oneLogin(String msg) {
 
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // TODO: inflate a fragment view
+        View rootView = super.onCreateView(inflater, container, savedInstanceState);
+        unbinder = ButterKnife.bind(this, rootView);
+        return rootView;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.rl_head:
+
+                break;
+            case R.id.tv_add_pasenger:
+
+                break;
+            case R.id.tv_change_password:
+                ChangePasswordActivity.launch(getActivity());
+                break;
+            case R.id.tv_logout:
+                MainActivity.launch(getActivity());
+                getActivity().finish();
+                break;
+        }
     }
 }
